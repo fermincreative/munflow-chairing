8d616784d35f28fd4e2c2b44b5b01a2c04f38e59
Florian Fermin <florian.fermin@fermincreative.com>
2017-12-18 13:48:07 +0100

    Universal JS for left hand menu

--- js/munflow-chairing.js
+++ js/munflow-chairing.js
@@ -1,3 +1,8 @@
-class chairing{
+function showItem(button) {
+var target=$('#'+ button +'').data('target');
+var current=$('#'+ button +'').data('current');
+$('#'+current+'').addClass('content-hidden');
+$('#'+target+'').removeClass('content-hidden');
+$('#left-sidebar button.standard').data("current",''+target+'');
 
 };
