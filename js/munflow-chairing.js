9278979621bd5e02e5bbef3a71bb9c2c6dfe9b82
Florian Fermin <florian.fermin@fermincreative.com>
2017-12-18 16:23:44 +0100

    Loop through form fields

--- js/munflow-chairing.js
+++ js/munflow-chairing.js
@@ -16,13 +16,32 @@ if (committee_settings === undefined) {
   });
 }
 
-function addDelegation(current_delegation_count){
-  current_delegation_count++;
-  $('#SetupModal #delegation-list .form-delegations').append('<input type="text" class="form-control" id="Delegation-'+current_delegation_count+'" data-delegation-number="'+current_delegation_count+'" placeholder="Delegation '+current_delegation_count+'">');
-  $('#SetupModal #add-delegation').attr('onclick','addDelegation('+current_delegation_count+')');
-}
+function addDelegation(delegation_count){
+  var new_delegation_count=delegation_count+1;
+  $('#SetupModal #delegation-list .form-delegations').append('<input type="text" class="form-control" id="Delegation-'+new_delegation_count+'" data-delegation-number="'+new_delegation_count+'" placeholder="Delegation '+new_delegation_count+'">');
+  $('#SetupModal #add-delegation').attr('onclick','addDelegation('+new_delegation_count+')');
+  $('#committee-settings #save').data('delegations',new_delegation_count);
+};
 
 $("#committee-settings #save").click(function(e){
-  var committee_name = $('#CommiteeName').val());
-  
+  var committee_name = $('#CommiteeName').val();
+  var delegation_count = $('#committee-settings #save').data('delegations');
+  var delegations_entered=0;
+  var committee_settings=[];
+  committee_settings["committee_name"]=committee_name;
+  committee_settings["delegations"]=[];
+  for (delegation = 0; delegation <= delegation_count; delegation++) {
+      var delegation_value=jQuery('#committee-settings .form-delegations #Delegation-'+delegation+'').val();
+      if (delegation_value === undefined) {
+
+      } else {
+        committee_settings["delegations"][''+delegations_entered+'']=delegation_value;
+        console.log(committee_settings["delegations"][delegations_entered]);
+        delegations_entered++;
+
+      }
+
+  }
+
+
 });
