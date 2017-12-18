a718ae8b41c1add830364879dc49aaf767751250
Florian Fermin <florian.fermin@fermincreative.com>
2017-12-18 14:34:41 +0100

    Define Modal Settings + addDelegation Functions

--- js/munflow-chairing.js
+++ js/munflow-chairing.js
@@ -9,5 +9,16 @@ $('#left-sidebar button.standard').data("current",''+target+'');
 
 var committee_settings=Cookies.get('committee_settings');
 if (committee_settings === undefined) {
-  $('#SetupModal').modal('show');
+  $('#SetupModal').modal({
+    show: true,
+   keyboard: false,
+   backdrop: 'static'
+  });
+}
+
+function addDelegation(current_delegation_count){
+  current_delegation_count++;
+  console.log(current_delegation_count);
+  $('#SetupModal #delegation-list .form-delegations').append('<input type="text" class="form-control" id="Delegation-'+current_delegation_count+'" data-delegation-number="'+current_delegation_count+'" placeholder="Delegation '+current_delegation_count+'">');
+  $('#SetupModal #add-delegation').attr('onclick','addDelegation('+current_delegation_count+')');
 }
