4bd92a002b6bfa1e50235cdd171fc44111a15450
Florian Fermin <florian.fermin@fermincreative.com>
2018-01-31 18:08:06 +0100

    Sample to start testing country logic

---
+++ js/countries.js
@@ -0,0 +1,8 @@
+var countries = new Object();
+country_name= Object();
+
+countries['ad']= { name: "Andorra", src: "/flags/4x3/ad.svg" }
+countries['ae']= { name: "United Arab Emirates", src: "/flags/4x3/ae.svg" }
+countries['af']= { name: "Afghanistan", src: "/flags/4x3/af.svg" }
+countries['ag']= { name: "Angola", src: "/flags/4x3/ag.svg" }
+countries['ai']= { name: "Anguilla", src: "/flags/4x3/ai.svg" }
