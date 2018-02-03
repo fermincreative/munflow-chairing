37318fada583071ad2286e3f4b768ce6249dd389
Florian Fermin <florian.fermin@fermincreative.com>
2018-02-03 22:13:44 +0100

    Adding countries to list

--- js/countries.js
+++ js/countries.js
@@ -1,5 +1,114 @@
 var countries = new Object();
-country_name= Object();
+countries = [
+  {
+  label : "Andorra",
+  value : "ad.svg"
+  },
+
+  {
+  label : "United Arab Emirates",
+  value : "ae.svg"
+  },
+
+  {
+  label : "Afghanistan",
+  value : "af.svg"
+  },
+
+  {
+  label : "Antigua and Barbuda",
+  value : "ag.svg"
+  },
+
+  {
+  label : "Anguilla",
+  value : "ai.svg"
+  },
+
+  {
+  label : "Albania",
+  value : "ai.svg"
+  },
+
+  {
+  label : "Armenia",
+  value : "am.svg"
+  },
+
+  {
+  label : "Argentina",
+  value : "ar.svg"
+  },
+
+  {
+  label : "American Samoa",
+  value : "as.svg"
+  },
+
+  {
+  label : "Austria",
+  value : "at.svg"
+  },
+
+  {
+  label : "Australia",
+  value : "au.svg"
+  },
+
+  {
+  label : "Azerbaijan",
+  value : "az.svg"
+  },
+
+  {
+  label : "Bosnia and Herzegovina",
+  value : "ba.svg"
+  },
+
+  {
+  label : "Barbados",
+  value : "bb.svg"
+  },
+
+  {
+  label : "Belgium",
+  value : "bd.svg"
+  },
+
+  {
+  label : "Burkina Faso",
+  value : "bf.svg"
+  },
+
+  {
+  label : "Bulgaria",
+  value : "bg.svg"
+  },
+
+  {
+  label : "Bahrain",
+  value : "bh.svg"
+  },
+
+  {
+  label : "Burundi",
+  value : "bi.svg"
+  },
+
+  {
+  label : "Bosnia and Herzegovina",
+  value : "ba.svg"
+  },
+
+
+
+
+
+
+
+
+
+ ]
 
 countries['ad']= { name: "Andorra", src: "/flags/4x3/ad.svg" }
 countries['ae']= { name: "United Arab Emirates", src: "/flags/4x3/ae.svg" }
